ae50d3b89de6da6e1322c49e34a9f41e5dd5caac
Sergey <pogrebnyak.vetal1996@gmail.com>
2022-11-08 14:56:08 +0200

    added live activity

---
+++ NotificationLiveActivity/NotificationLiveActivityBundle.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationLiveActivityBundle.swift
+//  NotificationLiveActivity
+//
+//  Created by Serhii on 08.11.2022.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NotificationLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        NotificationLiveActivity()
+        NotificationLiveActivityLiveActivity()
+    }
+}
